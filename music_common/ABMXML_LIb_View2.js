f9477271ffcaa4b55d308179a3a7d5769ba6ade9
Albert Hart <AlHart369@gmail.com>
2020-07-26 17:37:54 -0600

    try to update Lib_View

--- music_common/ABMXML_LIb_View2.js
+++ music_common/ABMXML_LIb_View2.js
@@ -1,9 +1,9 @@
 
 /* jslint esversion: 6,  maxerr: 100 */
 /* jshint eqeqeq: false */
-console.log("IN ABMXML_Lib_View.js");
+console.log("IN ABMXML_Lib_View2.js");
 
-MusicDOM.prototype.libs__loaded["ABMXML_Lib_View"] = "loaded";
+MusicDOM.prototype.libs__loaded["ABMXML_Lib_View2"] = "loaded";
 
 
 
