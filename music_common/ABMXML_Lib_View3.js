e9f27699666448c8d5eaed9553ee08f97eac0318
Albert Hart <AlHart369@gmail.com>
2020-07-26 18:12:47 -0600

    testing js

--- music_common/ABMXML_Lib_View3.js
+++ music_common/ABMXML_Lib_View3.js
@@ -4,3 +4,32 @@ console.log("IN ABMXML_Lib_View3B.js");
 
 MusicDOM.prototype.libs__loaded["ABMXML_Lib_View3B"] = "loaded";
 
+MusicDOM.prototype.view_params = {
+
+   
+    zoom: 1.0,
+
+    backendType: "canvas",
+
+    is_custom: true,   // are we loading from a string?
+
+
+    compactMode: false,
+
+    measureRangeStart: 0,
+
+    measureRangeEnd: Number.MAX_SAFE_INTEGER,
+
+    pageFormat: "Endless",
+    pageBackgroundColor: undefined,
+    singleHorizontalStaffline: false,
+
+    //pageFormat: MusicDOM.prototype.view_params.paramPageFormat ? 
+    //    MusicDOM.prototype.view_params.paramPageFormat : "Endless",
+
+    //pageBackgroundColor:  MusicDOM.prototype.view_params.paramPageBackgroundColor ?
+    //     "#" +  MusicDOM.prototype.view_params.paramPageBackgroundColor : undefined, // vexflow format, see OSMDOptions. can't use # in parameters.
+        
+    //singleHorizontalStaffline: (paramSingleHorizontalStaffline === '1'),
+
+};
--- music_common/AMMLIB_VIew.js
+++ music_common/AMMLIB_VIew.js
@@ -3,7 +3,7 @@
 /* jshint eqeqeq: false */
 console.log("IN AMMLIB_View.js");
 
-MusicDOM.prototype.libs__loaded["AMMLIB_View.js"] = "loaded";
+MusicDOM.prototype.libs__loaded["AMMLIB_View"] = "loaded";
 
 MusicDOM.prototype.view_params = {
 
